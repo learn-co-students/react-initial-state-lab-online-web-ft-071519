552346c3290d74f4df57510a9d84f08bf46e925e
BaronLip <baronlip@gmail.com>
2019-11-15 23:48:24 -0600

    Done.

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -10,7 +10,8 @@ export default class ImageSlider extends Component {
 
 
     render() {
-        console.log(this.state.currentSlideIndex)
+        // console.log(this.state.currentSlideIndex)
+        // This has the same result as the test but does not pass???
         return <span>I am on slide {this.state.currentSlideIndex}</span>
     }
 
